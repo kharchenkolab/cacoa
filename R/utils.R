0e90413b05b039218362fd79bc5d505f25ff36a5
viktor_petukhov <viktor.s.petuhov@ya.ru>
2020-07-22 06:33:34 -0400

    Fixed Rcpp package structure

--- R/utils.R
+++ R/utils.R
@@ -2,9 +2,14 @@
 #' @import ggplot2
 NULL
 
+#' @useDynLib cacoa
+NULL
+
 getColumnwiseCorrelations <- function(m1, m2) {
   common.cols <- intersect(colnames(m1), colnames(m2))
   return(lineup::corbetw2mat(m1[, common.cols], m2[, common.cols]))
 }
 
-
+.onUnload <- function (libpath) {
+  library.dynam.unload("cacoa", libpath)
+}
